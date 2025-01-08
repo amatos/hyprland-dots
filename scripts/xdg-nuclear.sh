822857e04652194e332bb1f7954b4c76f9e3fa37
Alberth Matos <alberth@matos.cc>
2025-01-08 10:58:39 -0500

    Cleanup to a more minimal install

--- scripts/xdg-nuclear.sh
+++ scripts/xdg-nuclear.sh
@@ -2,7 +2,10 @@
 
 sleep 1
 killall -e xdg-desktop-portal-hyprland
+killall xdg-desktop-portal-kde
+killall xdg-desktop-portal-gtk
 killall xdg-desktop-portal
 /usr/lib/xdg-desktop-portal-hyprland &
-sleep 2
+/usr/lib/xdg-desktop-portal-kde &
+sleep 1
 /usr/lib/xdg-desktop-portal &
