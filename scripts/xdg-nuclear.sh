235eacea5f4a94fe409af2b6153363bc8f19e93a
Alberth Matos <alberth@matos.cc>
2025-01-29 15:58:59 -0500

    Updated xdg-nuclear script
    Added killing gnome and cosmic portals
    Removed starting kde portal

--- scripts/xdg-nuclear.sh
+++ scripts/xdg-nuclear.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-sleep 1
 killall -e xdg-desktop-portal-hyprland
 killall xdg-desktop-portal-kde
+killall xdg-desktop-portal-gnome
+killall xdg-desktop-portal-cosmic
 killall xdg-desktop-portal-gtk
 killall xdg-desktop-portal
 /usr/lib/xdg-desktop-portal-hyprland &
-/usr/lib/xdg-desktop-portal-kde &
 sleep 1
 /usr/lib/xdg-desktop-portal &
