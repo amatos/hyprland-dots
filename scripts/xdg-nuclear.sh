7077f6c4aaff7f631ed8daacdd6fa79221017f03
Alberth Matos <alberth@matos.cc>
2025-02-18 19:02:25 -0500

    added gtk portal

--- scripts/xdg-nuclear.sh
+++ scripts/xdg-nuclear.sh
@@ -7,5 +7,6 @@ killall xdg-desktop-portal-cosmic
 killall xdg-desktop-portal-gtk
 killall xdg-desktop-portal
 /usr/lib/xdg-desktop-portal-hyprland &
+/usr/lib/xdg-desktop-portal-gtk &
 sleep 1
 /usr/lib/xdg-desktop-portal &
